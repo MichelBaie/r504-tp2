6924d57357b1454df9debfac838387fffd8a7d72
Tristan BRINGUIER <39345534+MichelBaie@users.noreply.github.com>
2025-10-07 14:21:30 +0200

    feat: FizzBuzz A – add affiche() skeleton
    provide empty function so tests fail on assertions instead of import error

---
+++ tp2/fizzbuzz.py
@@ -0,0 +1,9 @@
+"""
+FizzBuzz - Étape A (TDD)
+- affiche() doit IMPRIMER la concaténation de 1 à 100 avec:
+  3 -> "Fizz", 5 -> "Buzz", 15 -> "FrisBee".
+"""
+
+def affiche():
+    # Squelette initial pour faire tourner les tests (échouer sur l'assert, pas sur l'import)
+    pass
